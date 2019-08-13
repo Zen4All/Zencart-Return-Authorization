376b2ecb8c54fca8af686406862625e017dd5619
tmccaff <iridude@aol.com>
2019-08-13 10:01:06 -0400

    Update Copyright

--- install_files/includes/templates/YOUR_TEMPLATE/templates/tpl_returns_request_default.php
+++ install_files/includes/templates/YOUR_TEMPLATE/templates/tpl_returns_request_default.php
@@ -6,7 +6,7 @@
  * Displays information related to a single specific order
  *
  * @package templateSystem
- * @copyright Copyright 2003-2006 Zen Cart Development Team
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: J_Schilz for Integrated COWOA - 14 April 2007
