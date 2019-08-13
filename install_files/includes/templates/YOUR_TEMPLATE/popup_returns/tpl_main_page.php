2e5f5f8baee727fdf2319f6d0018a8b01d80fb35
tmccaff <iridude@aol.com>
2019-08-13 10:02:02 -0400

    Update copyright

--- install_files/includes/templates/YOUR_TEMPLATE/popup_returns/tpl_main_page.php
+++ install_files/includes/templates/YOUR_TEMPLATE/popup_returns/tpl_main_page.php
@@ -3,7 +3,7 @@
  * tpl_main_page.php
  *
  * @package templateSystem
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: tpl_main_page.php 2870 2006-01-21 21:36:02Z birdbrain $
