55bd6bac701d5d07c1af5fe0fe91b5a22b93fad4
tmccaff <iridude@aol.com>
2019-08-13 10:04:32 -0400

    Update Copyright

--- install_files/includes/modules/pages/returns/header_php.php
+++ install_files/includes/modules/pages/returns/header_php.php
@@ -2,7 +2,7 @@
 /**
  * Flexible Returns Authorization (RMA) Page
  * @package templateSystem
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2003-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: returns_defines.php 2.3.3 4/13/2010 Clyde Jones $
