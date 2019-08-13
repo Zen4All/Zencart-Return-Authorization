5542efa2642dc4d12e9b799ed7dfe373971abf34
tmccaff <iridude@aol.com>
2019-08-13 04:32:53 -0400

    Moved from account_history

--- install_files/includes/languages/english/extra_definitions/returns_request.php
+++ install_files/includes/languages/english/extra_definitions/returns_request.php
@@ -14,3 +14,4 @@ define('TEXT_ACCOUNT_INFO_RETURNS_TEXT_LINK_HEADER', 'Returns');
 define('TEXT_DEFINE_BUTTON_LINK2', 'Click here');
 define('TEXT_DEFINE_BUTTON_LINK3', ' to create an RMA.');
 define('TEXT_RETURN_GRACE_PERIOD_EXPIRED', 'The item(s) on this order extend past our <strong>' . RETURN_GRACE_PERIOD . ' Day</strong>' . '<a href="' . zen_href_link(FILENAME_SHIPPING, '', 'SSL') . '">' . ' Return Policy' . '</a>' . '. Please ' . '<a href="' . zen_href_link(FILENAME_CONTACT_US, '', 'SSL') . '">' . 'contact us' . '</a>' . ' for any further inquiries.');
+define('TEXT_RETURN_INFO_FROM_RETURNS_REQUEST_PAGE', '<h2>Return Authorization Request</h2>To submit a Return Authorization Request please select the order for which the items you want to return are on.');
