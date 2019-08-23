92072261a7997f3689ddf37bfe09a13dff1462a0
tmccaff <iridude@aol.com>
2019-08-23 17:23:00 -0400

    Update version 1.6

--- install_files/YOUR_ADMIN/includes/extra_datafiles/return_authorization_defines.php
+++ install_files/YOUR_ADMIN/includes/extra_datafiles/return_authorization_defines.php
@@ -2,14 +2,14 @@
 /**
 * Flexible Returns Authorization (RMA)
  *
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2009-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: return_authorization_defines.php 3.1 7/15/2012 C Jones $
+ * @version $Id: return_authorization_defines.php 3.2 08/23/2019 tmccaff $
  */
 
 // Admin Menu Boxes
 define('BOX_CONFIGURATION_RETURN_AUTH', 'Return Authorization');
 // DO NOT EDIT!
-define('RETURN_AUTHORIZATION_VERSION', '1.0');
+define('RETURN_AUTHORIZATION_VERSION', '1.6');
 // DO NOT EDIT!
