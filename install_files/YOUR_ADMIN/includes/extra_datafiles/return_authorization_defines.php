ac44e2554d7843049ccf92fa0f5edc62df424df3
tmccaff <iridude@aol.com>
2019-08-23 18:58:49 -0400

    Update Version 1.6a

--- install_files/YOUR_ADMIN/includes/extra_datafiles/return_authorization_defines.php
+++ install_files/YOUR_ADMIN/includes/extra_datafiles/return_authorization_defines.php
@@ -5,11 +5,11 @@
  * @copyright Copyright 2009-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: return_authorization_defines.php 3.2 08/23/2019 tmccaff $
+ * @version $Id: return_authorization_defines.php 3.3 08/23/2019 tmccaff $
  */
 
 // Admin Menu Boxes
 define('BOX_CONFIGURATION_RETURN_AUTH', 'Return Authorization');
 // DO NOT EDIT!
-define('RETURN_AUTHORIZATION_VERSION', '1.6');
+define('RETURN_AUTHORIZATION_VERSION', '1.6a');
 // DO NOT EDIT!
