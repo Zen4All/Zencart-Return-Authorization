b070d94d074df704f9fdd2369570c916b24493e8
tmccaff <iridude@aol.com>
2019-08-23 18:54:50 -0400

    Update Copyright

--- install_files/YOUR_ADMIN/includes/auto_loaders/config.ra.php
+++ install_files/YOUR_ADMIN/includes/auto_loaders/config.ra.php
@@ -2,7 +2,7 @@
 /**
  * Flexible Returns Authorization (RMA)
  *
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2009-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: config.ra.php 3.1 7/15/2012 C Jones $
