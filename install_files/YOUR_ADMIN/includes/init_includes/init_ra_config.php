90d92d35804274b4b12a82941902dadbc9d1febb
tmccaff <iridude@aol.com>
2019-08-23 18:55:24 -0400

    Update Copyright

--- install_files/YOUR_ADMIN/includes/init_includes/init_ra_config.php
+++ install_files/YOUR_ADMIN/includes/init_includes/init_ra_config.php
@@ -4,7 +4,7 @@
  * For Zen-Cart 1.5.6
  * Last Updated: 08/12/2019
  *
- * @copyright Copyright 2003-2005 Zen Cart Development Team
+ * @copyright Copyright 2009-2019 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: init_ra_config.php 3.2 08/12/2019 T McCaffery $
